84e3adc56e6b3e211ff7cc970c8d5e6ccee02891
vitor <vitor.psantos1999@gmail.com>
2026-04-17 17:22:47 -0300

    remove arquivos antigos

--- prisma/migrations/20250816195115_add_unique_to_fabricante_nome/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nome]` on the table `Fabricante` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Fabricante_nome_key` ON `Fabricante`(`nome`);
